2f4b34831d5a2ca96debe9ec299b299ab2e28216
Nikolay Mikhaylov <sonny@milton.pro>
2025-12-27 17:42:42 +0700

    Add web profiler bundle as requirment

--- src/VibedebugBundle.php
+++ src/VibedebugBundle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Milton\VibedebugBundle;
 
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -15,6 +16,10 @@ final class VibedebugBundle extends AbstractBundle
      */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
+        if (!$builder->hasExtension('web_profiler')) {
+            throw new InvalidConfigurationException('VibedebugBundle requires symfony/web-profiler-bundle to be installed and enabled.');
+        }
+
         $container->import(__DIR__.'/../config/services.yaml');
     }
 }
